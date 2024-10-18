2fbdd564eea821fa2c174f2ff2475cc923e0a90c
Mangesh Sadanand Jagtap <157835971+Mangesh1124@users.noreply.github.com>
2024-10-19 01:36:25 +0530

    Add HTML and CSS files

---
+++ one.js
@@ -0,0 +1,15 @@
+alert("HEY THERE ENTER THE NUM VALUE")
+
+let a = prompt("ENTER HERE",44)
+
+let write = confirm("DO YOU WANT TO WRITE THE GIVEN NO.")
+if(write)
+    {
+        document.write(a)
+    }     
+
+    else
+    {
+        alert("GP BAS LAVDYA ")
+    }
+
